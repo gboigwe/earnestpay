3bd527f21c5398cdfcb6dc96b8d8e4d556100444
Gbolahan Akande <geakande@gmail.com>
2026-01-21 06:55:07 +0100

    feat: add components export index

---
+++ frontend/frontend/components/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Components exports
+ * Central export point for all components
+ */
+
+// Gas optimization components
+export { GasFeeDisplay, GasFeeDisplayCompact } from './GasFeeDisplay';
+export { GasEstimator, BatchGasEstimator } from './GasEstimator';
+
+// Wallet components
+export { WalletConnectButton } from './WalletConnectButton';
+export { WalletStatus, WalletStatusCompact } from './WalletStatus';
+export { NetworkGuard, NetworkWarning } from './NetworkGuard';
+
+// Chain components
+export { ChainSelector } from './ChainSelector';
